091e5c930bc9e46ff3fc4e1146eb4be9cb4470d0
jtamayagk1 <jamaya614@yahoo.com>
2024-04-07 09:27:07 -0500

    first commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,27 @@
+// components/Navbar.js
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className='bg-gray-800 p-4 flex justify-end'>
+      <ul className='flex space-x-4 mx-6'>
+        <li className='navItem'>
+          <Link href="/">
+            Home
+          </Link>
+        </li>
+        <li className='navItem'>
+          <Link href="/about">
+            About
+          </Link>
+        </li>
+        <li className='navItem'>
+          <Link href="/history">
+            History
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
---
+++ src/app/history/page.tsx
@@ -0,0 +1,7 @@
+export default function History(){
+    return(
+        <>
+            <h1>Workout History</h1>
+        </>
+    )
+}
